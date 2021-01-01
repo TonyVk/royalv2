d9701095d0f284d56e19adde816389db2b2327e8
Sikora <toni.sikora@live.com>
2021-01-01 02:41:36 +0100

    Server backup : 2:41:36.49

--- resources/prodajoruzje/client/main.lua
+++ resources/prodajoruzje/client/main.lua
@@ -212,9 +212,10 @@ AddEventHandler('prodajoruzje:petarde', function()
 				cordsa.y, 
 				cordsa.z, 
 				18, 
-				0.5, 
+				0.0, 
 				true, 
 				false, 
+				false,
 				false
 			)
 			DeleteEntity(prop)
@@ -278,6 +279,20 @@ AddEventHandler('esx:deleteVehicle2', function()
         DeleteEntity(entity)
     end 
 end)
+
+RegisterCommand("obrisivatromet", function(source, args, rawCommandString)
+	ESX.TriggerServerCallback('esx-races:DohvatiPermisiju', function(br)
+		if br == 1 then
+			local NewBin, NewBinDistance = ESX.Game.GetClosestObject("ind_prop_firework_03")
+			if NewBinDistance <= 3 then
+				ESX.Game.DeleteObject(NewBin)
+				SetEntityCoords(NewBin, 0, 0, 0, 1, 0, 0, 1)
+			end
+		else
+			ESX.ShowNotification("Nemate pristup ovoj komandi!")
+		end
+	end)
+end, false)
 
 RegisterCommand("obrisikontenjer", function(source, args, rawCommandString)
 	ESX.TriggerServerCallback('esx-races:DohvatiPermisiju', function(br)
