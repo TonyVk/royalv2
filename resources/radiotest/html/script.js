809e6f02c017f51174b0b463efd39bd78bb357fc
Sikora <toni.sikora@live.com>
2021-03-01 22:13:35 +0100

    Server backup :22:13:35.07

--- resources/radiotest/html/script.js
+++ resources/radiotest/html/script.js
@@ -2,13 +2,15 @@ var mount = "testingstream.mp3"; // your mount point
 var audiotype = "audio/mp3"; // dont change this if u dont know what you're doing, best to leave like this
 
 $(function(){
-    $("#audio").attr("src","http://live.okradio.net:8000/;stream.mp3");
+    //$("#audio").attr("src","http://live.okradio.net:8000/;stream.mp3");
+	$("#audio").attr("src","http://streams.extrafm.hr:8110/;");
     $("#audio").attr("type",audiotype);
     window.addEventListener('message', function(event){
         if (event.data.type=="enable") {
             var audio = $("#audio")[0];
             if(event.data.state) {
-                $("#audio").attr("src","http://live.okradio.net:8000/;stream.mp3");
+                //$("#audio").attr("src","http://live.okradio.net:8000/;stream.mp3");
+				$("#audio").attr("src","http://streams.extrafm.hr:8110/;");
                 audio.load();
 				audio.volume = 0.01;
                 audio.play();
