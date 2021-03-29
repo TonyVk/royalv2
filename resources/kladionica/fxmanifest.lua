674da5551d63eb08daebd2c228081db817423215
Sikora <toni.sikora@live.com>
2021-03-29 23:36:08 +0200

    Server backup :23:36:08.12

---
+++ resources/kladionica/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'bodacious'
+game 'gta5'
+
+
+description 'ESX Oglasi'
+
+version '1.0.0'
+
+ui_page "index.html"
+
+files {
+	"index.html",
+	"assets/plugins/bootstrap-3.3.7-dist/css/bootstrap.css",
+	"assets/plugins/jquery/jquery-3.2.1.min.js",
+	"assets/plugins/bootstrap-3.3.7-dist/js/bootstrap.min.js"
+}
+
+server_scripts {
+  '@mysql-async/lib/MySQL.lua',
+  'server/main.lua'
+}
+
+client_scripts {
+  'client/main.lua'
+}
