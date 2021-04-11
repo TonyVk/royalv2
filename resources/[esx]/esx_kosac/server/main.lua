b6202afadf0efee81024ea3acf475fcf3bf17935
Sikora <toni.sikora@live.com>
2021-04-12 00:06:15 +0200

    Server backup : 0:06:15.01

--- resources/[esx]/esx_kosac/server/main.lua
+++ resources/[esx]/esx_kosac/server/main.lua
@@ -6,8 +6,8 @@ AddEventHandler('kosaaac:platituljanu', function()
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
     if xPlayer.job.name == 'kosac' then
-		xPlayer.addMoney(34)
-		TriggerEvent("biznis:StaviUSef", "kosac", math.ceil(34*0.30))
+		xPlayer.addMoney(24)
+		TriggerEvent("biznis:StaviUSef", "kosac", math.ceil(24*0.30))
 	else
         TriggerEvent("DiscordBot:Anticheat", GetPlayerName(_source).."[".._source.."] je pokusao pozvati event za novac kosaca, a nije zaposlen kao kosac!")
 	    TriggerEvent("AntiCheat:Citer", _source)
@@ -19,8 +19,8 @@ AddEventHandler('kosaaac:platituljanu2', function()
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	if xPlayer.job.name == 'kosac' then
-		xPlayer.addMoney(15)
-		TriggerEvent("biznis:StaviUSef", "kosac", math.ceil(15*0.30))
+		xPlayer.addMoney(10)
+		TriggerEvent("biznis:StaviUSef", "kosac", math.ceil(10*0.30))
 	else
         TriggerEvent("DiscordBot:Anticheat", GetPlayerName(_source).."[".._source.."] je pokusao pozvati event za novac kosaca, a nije zaposlen kao kosac!")
 	    TriggerEvent("AntiCheat:Citer", _source)
